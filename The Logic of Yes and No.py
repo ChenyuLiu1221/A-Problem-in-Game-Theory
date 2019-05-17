3fbce569a472e9465808bb227c0db815104c001c
Chenyu <42542471+Chenyu-Chenyu@users.noreply.github.com>
2019-05-17 21:11:42 +0800

    Update The Logic of Yes and No.py

--- The Logic of Yes and No.py
+++ The Logic of Yes and No.py
@@ -8,7 +8,7 @@ Formulation of the Problem:
 There are two integers ranging from 1 to 30. Suppose Alice knows the sum of these two integers while Bob knows the product of them. 
 Bob says: "I have no idea of the numbers." Alice then says: "I cannot determine them as well." 
 "I get the answer!", Bob immediately follows. "I get it too!"Alice also cracks the problem. 
-Question: what are the numbers given to Alice and Bob.
+Question: what are the numbers given to Alice and Bob. (Suppose x and y could be the same number)
 
 Algorithm:
 The basic idea is to change the postulates of inferences along with the different answers. 
